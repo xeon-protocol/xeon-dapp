bdfa1f11bb7d56d4df8b5c9aa175c0f6a8e0249e
rtj99 <ryantjena@gmail.com>
2024-07-01 00:42:57 +0200

    init commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -105,3 +105,131 @@ function Footer() {
 }
 
 export default Footer;
+
+import { Box, Image } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+
+function Footer() {
+  return (
+    <Box position="relative" width="100%">
+      <Image src="/line_footer.webp" alt="line footer" width="100%" />
+      <Box
+        position="absolute"
+        bottom="0"
+        left="0"
+        right="0"
+        zIndex="0"
+        height="300px"
+        backgroundImage="url('/ellipse.webp')"
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+        backgroundPosition="center"
+      />
+      <Box
+        py={20}
+        className=" z-2 w-full flex flex-wrap"
+        position="relative"
+        zIndex="2"
+      >
+        <Box
+          className="w-full md:w-1/2 p-4"
+          bg="blue.500"
+          color="white"
+          textAlign="left"
+        >
+          <p>Xeon Protocol.</p>
+          <p>ERC20 Hedging & Lending Ecosystem.</p>
+          <Box className="flex items-center gap-4 mt-5">
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image src="/x.webp" alt="twitter" />
+            </a>
+            <a
+              href="https://medium.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image src="/medium.webp" alt="medium" />
+            </a>
+            <a
+              href="https://telegram.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image src="/telegram.webp" alt="telegram" />
+            </a>
+            <a href="mailto:example@example.com">
+              <Image src="/mail.webp" alt="mail" />
+            </a>
+          </Box>
+        </Box>
+        <Box
+          className="w-full md:w-1/6 p-4"
+          bg="green.500"
+          color="white"
+          textAlign="left"
+        >
+          <p className="mb-2"> {`{ READ }`}</p>
+          <li className="mb-2">
+            {" "}
+            <Link href="/documentation">Documentation</Link>
+          </li>
+
+          <li className="mb-2">
+            <Link href="/whitepaper">Whitepaper</Link>
+          </li>
+          <li className="mb-2">
+            <Link href="/medium">Medium</Link>
+          </li>
+        </Box>
+        <Box
+          className="w-full md:w-1/6 p-4"
+          bg="red.500"
+          color="white"
+          textAlign="left"
+        >
+          <p className="mb-2"> {`{ ENGINEERING }`}</p>
+          <li className="mb-2">
+            {" "}
+            <Link href="/github">Github Repo</Link>
+          </li>
+
+          <li className="mb-2">
+            <Link href="/smart-contracts">Smart Contracts</Link>
+          </li>
+          <li className="mb-2">
+            <Link href="/security-audits">Secutity Audits</Link>
+          </li>
+        </Box>
+        <Box
+          className="w-full md:w-1/6 p-4"
+          bg="purple.500"
+          color="white"
+          textAlign="left"
+        >
+          <p className="mb-2"> {`{ REVENUE SHARING }`}</p>
+          <li className="mb-2">
+            {" "}
+            <Link href="/protocol-fees">Protocol Fees</Link>
+          </li>
+
+          <li className="mb-2">
+            <Link href="/ways-to-earn">Ways to Earn</Link>
+          </li>
+          <li className="mb-2">
+            <Link href="/miner-program">Miner Program</Link>
+          </li>
+        </Box>
+      </Box>
+      <Box className="w-full text-center text-white p-4 border-t-[1px] border-[#6c6c6c]">
+        <p> Xeon Protocol © All rights reserved. 2024.</p>
+      </Box>
+    </Box>
+  );
+}
+
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -57,8 +57,36 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "button-gradient": "linear-gradient(135deg, #3758FF 0%, #2447F2 100%)",
+      },
+      borderImage: {
+        "gradient-to-transparent":
+          "linear-gradient(to bottom, #f2f2f2, transparent) 1",
+      },
+      colors: {
+        dark: "#181818",
+        black: "#000000",
+        fern: "#dadada",
+        grey: "#f2f2f2",
+        "light-blue": "#5b62f4",
+        blue: "#4a64e5",
+        muted: "#71637F",
+        "deep-purple": "#470c85",
+        "light-purple": "#aa79e5",
+        lime: "#a0ee6a",
+        orange: "#ffa64",
+        floral: "#e5456d",
+        turquoise: "#68dbff",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".border-gradient-to-transparent": {
+          borderImage: "linear-gradient(to bottom, #f2f2f2, transparent) 1",
+        },
+      });
+    },
+  ],
 };
