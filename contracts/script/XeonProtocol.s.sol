d247a0fe86d25bed0e0ca7db0249e9c0cb3904e8
Jon <jon@xeon-protocol.io>
2024-07-14 01:35:58 -0400

    chore: refactor scripts

---
+++ contracts/script/XeonProtocol.s.sol
@@ -0,0 +1,8 @@
+// run TestnetHelper.s.sol
+// obtain contracts from TestnetHelper deployment
+// ---
+// deploy hedging
+// deploy staking
+// deploy XEON token
+// deploy price feed
+// init price feed
