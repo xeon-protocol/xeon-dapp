0e76b8d9a38a69d8cd4d6cdc8378e8c7ad5b033b
rtj99 <ryantjena@gmail.com>
2024-07-16 00:33:29 +0200

    styled the widget to a card

--- src/components/Loans.js
+++ src/components/Loans.js
@@ -5,7 +5,8 @@ function Loans() {
     <div>
       {" "}
       <TellerWidget
-        buttonClassName="text-white bg-floral flex justify-center items-center flex p-2 w-full rounded-full border-t-none border-b-[1px] border-r-[1px] border-l-[1px] border-button-gradient hover:bg-purple hover:border-blue"
+        showModalByDefault
+        buttonClassName="lg:hidden md:block hidden 2xl:hidden text-white bg-floral flex justify-center items-center flex p-2 w-full rounded-full border-t-none border-b-[1px] border-r-[1px] border-l-[1px] border-button-gradient hover:bg-purple hover:border-blue"
         isBareButton
       />
       ;
