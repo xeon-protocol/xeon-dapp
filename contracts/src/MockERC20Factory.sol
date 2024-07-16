406fb7df17bd4ab67efef0add8be5d9a5e1a5983
Jon <jon@xeon-protocol.io>
2024-07-16 19:35:14 -0400

    test: update unit test coverage on ERC20 factory

--- contracts/src/MockERC20Factory.sol
+++ contracts/src/MockERC20Factory.sol
@@ -30,16 +30,29 @@ contract MockERC20 is ERC20, AccessControl {
 contract MockERC20Factory is AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
+    struct TokenInfo {
+        address tokenAddress;
+        string name;
+        string symbol;
+        uint8 decimals;
+        uint256 totalSupply;
+    }
+
     event NewTokenDeployed(address indexed token, string name, string symbol, uint8 decimals, uint256 initialSupply);
+
+    address[] public admins;
+    TokenInfo[] public tokens;
 
     constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
+        admins.push(msg.sender);
     }
 
     function addAdmin(address admin) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "MockERC20Factory: must have default admin role to add admin");
         grantRole(ADMIN_ROLE, admin);
+        admins.push(admin);
     }
 
     function removeAdmin(address admin) external {
@@ -47,6 +60,15 @@ contract MockERC20Factory is AccessControl {
             hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "MockERC20Factory: must have default admin role to remove admin"
         );
         revokeRole(ADMIN_ROLE, admin);
+
+        // Remove admin from the admins array
+        for (uint256 i = 0; i < admins.length; i++) {
+            if (admins[i] == admin) {
+                admins[i] = admins[admins.length - 1];
+                admins.pop();
+                break;
+            }
+        }
     }
 
     function deploy(string memory name, string memory symbol, uint8 decimals, uint256 initialSupply)
@@ -55,7 +77,65 @@ contract MockERC20Factory is AccessControl {
     {
         require(hasRole(ADMIN_ROLE, msg.sender), "MockERC20Factory: must have admin role to deploy token");
         MockERC20 token = new MockERC20(name, symbol, decimals, initialSupply, msg.sender);
+        tokens.push(
+            TokenInfo({
+                tokenAddress: address(token),
+                name: name,
+                symbol: symbol,
+                decimals: decimals,
+                totalSupply: initialSupply
+            })
+        );
         emit NewTokenDeployed(address(token), name, symbol, decimals, initialSupply);
         return address(token);
+    }
+
+    /**
+     * @notice return list of admin addresses
+     */
+    function getAdmins() external view returns (address[] memory) {
+        return admins;
+    }
+
+    /**
+     * @notice return list of deployed tokens with their details
+     */
+    function getTokens() external view returns (TokenInfo[] memory) {
+        return tokens;
+    }
+
+    function getTotalSupply(address tokenAddress) external view returns (uint256) {
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (tokens[i].tokenAddress == tokenAddress) {
+                return ERC20(tokenAddress).totalSupply();
+            }
+        }
+        revert("Token not found");
+    }
+
+    /**
+     * @notice returns list of addresses holding a specific token
+     */
+    function getTokenHolders(address tokenAddress) external view returns (address[] memory) {
+        uint256 count = 0;
+        uint256 index = 0;
+
+        // Count the number of holders
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (tokens[i].tokenAddress == tokenAddress) {
+                count++;
+            }
+        }
+
+        address[] memory holders = new address[](count);
+
+        // Collect the holders' addresses
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (tokens[i].tokenAddress == tokenAddress) {
+                holders[index] = tokens[i].tokenAddress;
+                index++;
+            }
+        }
+        return holders;
     }
 }
--- contracts/test/MockERC20Factory.t.sol
+++ contracts/test/MockERC20Factory.t.sol
@@ -3,23 +3,34 @@ pragma solidity 0.8.20;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {MockERC20, MockERC20Factory} from "../src/MockERC20Factory.sol";
-//import {ClaimHelper} from "../src/ClaimHelper.sol";
 
+/**
+ * @notice unit tests for MockERC20 implementation in MockERC20Factory
+ */
 contract MockERC20Test is Test {
     MockERC20 public token;
     address public admin = address(0x1);
 
+    /**
+     * @notice initialize, set token params + admin
+     */
     function setUp() public {
         token = new MockERC20("MockToken", "MTK", 18, 1_000_000 * 10 ** 18, admin);
     }
 
-    function test_initialSupply() public view {
+    /**
+     * @notice verify initial supply
+     */
+    function test_initialSupply() public {
         console2.log("Testing initial supply...");
         uint256 expectedSupply = 1_000_000 * 10 ** 18;
         uint256 actualSupply = token.totalSupply();
         assertEq(expectedSupply, actualSupply, "Initial supply should be 1_000_000 MTK");
     }
 
+    /**
+     * @notice test minting functionality
+     */
     function test_minting() public {
         console2.log("Testing minting...");
         token.grantRole(token.MINTER_ROLE(), admin);
@@ -30,6 +41,9 @@ contract MockERC20Test is Test {
         assertEq(expectedSupply, actualSupply, "Total supply should be 1_500_000 MTK after minting");
     }
 
+    /**
+     * @notice test transfer
+     */
     function test_transfer() public {
         console2.log("Testing transfer...");
         address to = address(0x2);
@@ -53,6 +67,9 @@ contract MockERC20FactoryTest is Test {
         vm.stopPrank();
     }
 
+    /**
+     * @notice test creating token from factory
+     */
     function test_createToken() public {
         console2.log("Testing create token...");
         vm.prank(admin);
@@ -65,21 +82,27 @@ contract MockERC20FactoryTest is Test {
         console2.log("Token created at:", tokenAddress);
     }
 
+    /**
+     * @notice test minting more tokens after initial supply
+     */
     function test_mintAdditionalTokens() public {
         console2.log("Testing mint additional tokens...");
         vm.prank(admin);
         address tokenAddress = mockERC20Factory.deploy("TestToken", "TTK", 18, 1_000_000 * 10 ** 18);
         MockERC20 token = MockERC20(tokenAddress);
-
+        // set admin
         token.grantRole(token.MINTER_ROLE(), admin);
         vm.prank(admin);
         token.mint(admin, 500_000 * 10 ** 18);
-
+        // ensure updated supply matches
         uint256 expectedSupply = 1_500_000 * 10 ** 18;
         uint256 actualSupply = token.totalSupply();
         assertEq(expectedSupply, actualSupply, "Total supply should be 1_500_000 TTK after minting additional tokens");
     }
 
+    /**
+     * @notice test setting admin
+     */
     function test_setAdminRole() public {
         console2.log("Testing set admin role...");
         vm.prank(admin);
@@ -89,6 +112,10 @@ contract MockERC20FactoryTest is Test {
         assertTrue(hasAdminRole, "Non-admin should be granted admin role");
     }
 
+    /**
+     * @notice test that only admin can mint
+     * @notice expect revert if non-admin tries to mint
+     */
     function test_onlyAdminCanMint() public {
         console2.log("Testing only admin can mint...");
         vm.prank(admin);
@@ -100,10 +127,80 @@ contract MockERC20FactoryTest is Test {
         token.mint(nonAdmin, 500_000 * 10 ** 18);
     }
 
+    /**
+     * @notice test that only admin can deploy token
+     * @notice expect revert if non-admin tries to deploy
+     */
     function test_nonAdminCannotCreateToken() public {
         console2.log("Testing non-admin cannot create token...");
         vm.prank(nonAdmin);
         vm.expectRevert("MockERC20Factory: must have admin role to deploy token");
         mockERC20Factory.deploy("FailToken", "FTK", 18, 1_000_000 * 10 ** 18);
+    }
+
+    /**
+     * @notice test getting admin addresses
+     */
+    function test_getAdmins() public {
+        console2.log("Testing get admins...");
+        vm.prank(admin);
+        mockERC20Factory.addAdmin(nonAdmin);
+
+        address[] memory admins = mockERC20Factory.getAdmins();
+        assertEq(admins.length, 2, "There should be 2 admins");
+        assertEq(admins[0], admin, "First admin should be the original admin");
+        assertEq(admins[1], nonAdmin, "Second admin should be the newly added admin");
+    }
+
+    /**
+     * @notice test getting tokens
+     */
+    function test_getTokens() public {
+        console2.log("Testing get tokens...");
+        vm.prank(admin);
+        address tokenAddress = mockERC20Factory.deploy("TestToken1", "TTK1", 18, 1_000_000 * 10 ** 18);
+        mockERC20Factory.deploy("TestToken2", "TTK2", 18, 2_000_000 * 10 ** 18);
+
+        MockERC20Factory.TokenInfo[] memory tokens = mockERC20Factory.getTokens();
+        assertEq(tokens.length, 2, "There should be 2 tokens");
+        assertEq(tokens[0].tokenAddress, tokenAddress, "First token address should match");
+        assertEq(tokens[0].name, "TestToken1", "First token name should be 'TestToken1'");
+        assertEq(tokens[0].symbol, "TTK1", "First token symbol should be 'TTK1'");
+        assertEq(tokens[0].decimals, 18, "First token decimals should be 18");
+        assertEq(tokens[0].totalSupply, 1_000_000 * 10 ** 18, "First token total supply should be 1_000_000 TTK1");
+
+        assertEq(tokens[1].name, "TestToken2", "Second token name should be 'TestToken2'");
+        assertEq(tokens[1].symbol, "TTK2", "Second token symbol should be 'TTK2'");
+        assertEq(tokens[1].totalSupply, 2_000_000 * 10 ** 18, "Second token total supply should be 2_000_000 TTK2");
+    }
+
+    /**
+     * @notice test getting total supply for a token
+     */
+    function test_getTotalSupply() public {
+        console2.log("Testing get total supply...");
+        vm.prank(admin);
+        address tokenAddress = mockERC20Factory.deploy("TestToken", "TTK", 18, 1_000_000 * 10 ** 18);
+
+        uint256 totalSupply = mockERC20Factory.getTotalSupply(tokenAddress);
+        assertEq(totalSupply, 1_000_000 * 10 ** 18, "Total supply should be 1_000_000 TTK");
+    }
+
+    /**
+     * @notice test getting token holders
+     */
+    function test_getTokenHolders() public {
+        console2.log("Testing get token holders...");
+        vm.prank(admin);
+        address tokenAddress = mockERC20Factory.deploy("TestToken", "TTK", 18, 1_000_000 * 10 ** 18);
+        MockERC20 token = MockERC20(tokenAddress);
+
+        vm.prank(admin);
+        token.transfer(nonAdmin, 500_000 * 10 ** 18);
+
+        address[] memory holders = mockERC20Factory.getTokenHolders(tokenAddress);
+        assertEq(holders.length, 2, "There should be 2 token holders");
+        assertEq(holders[0], admin, "First holder should be the admin");
+        assertEq(holders[1], nonAdmin, "Second holder should be the non-admin");
     }
 }
