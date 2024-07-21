54e372bdeaccd9335af29d8b33e67e9dfb3667c8
Jon <jon@xeon-protocol.io>
2024-07-21 16:23:55 -0400

    chore: add keys to `constants.js`
    added XEON token address value for Ethereum mainnet
    added placeholder values for relevant contracts

--- src/abis/constants.js
+++ src/abis/constants.js
@@ -1,26 +1,36 @@
 // global contract addresses
 export const Constants = {
   testnet: {
-    network: "Base Sepolia",
+    network: 'Base Sepolia',
     chainId: 84532,
-    baseSepoliaScan: "https://sepolia.basescan.org",
-    baseSepoliaScanAPI: "https://api-sepolia.basescan.org/api",
-    onboardingUtilsContractAddress: "0x88232810891E015161D3e57C1E5A5DA2376407d5",
-    MockERC20FactoryContractAddress: "0x5A0d5390c45b49505C43A56DA4A4f89b93023F11",
-    WETH: "0x395cB7753B02A15ed1C099DFc36bF00171F18218",
+    baseSepoliaScan: 'https://sepolia.basescan.org',
+    baseSepoliaScanAPI: 'https://api-sepolia.basescan.org/api',
+    onboardingUtilsContractAddress:
+      '0x88232810891E015161D3e57C1E5A5DA2376407d5',
+    MockERC20FactoryContractAddress:
+      '0x5A0d5390c45b49505C43A56DA4A4f89b93023F11',
+    WETH: '0x395cB7753B02A15ed1C099DFc36bF00171F18218',
   },
   ethereum: {
-    network: "Ethereum",
+    network: 'Ethereum',
     chainId: 1,
-    baseMainnetScan: "https://etherscan.io",
-    baseMainnetScanAPI: "https://api.etherscan.io/api",
-    WETH: "0xMainnetWETHAddressHere",
+    baseMainnetScan: 'https://etherscan.io',
+    baseMainnetScanAPI: 'https://api.etherscan.io/api',
+    WETH: '0xMainnetWETHAddressHere',
+    XeonToken: '0x8d65a2eaBDE4B31cbD7E43F27E47559d1CCec86c',
+    XeonHedging: '0xXeonHedgingEthereumAddress',
+    XeonStaking: '0xXeonStakingEthereumAddress',
+    PriceOracle: '0xPriceOracleEthereumAddress',
   },
   base: {
-    network: "Base",
+    network: 'Base',
     chainId: 8453,
-    baseMainnetScan: "https://basescan.org",
-    baseMainnetScanAPI: "https://api.basescan.org/api",
-    WETH: "0xMainnetWETHAddressHere",
-  }
+    baseMainnetScan: 'https://basescan.org',
+    baseMainnetScanAPI: 'https://api.basescan.org/api',
+    WETH: '0xMainnetWETHAddressHere',
+    XeonToken: '0xXeonTokenBaseAddress',
+    XeonHedging: '0xXeonHedgingBaseAddress',
+    XeonStaking: '0xXeonStakingBaseAddress',
+    PriceOracle: '0xPriceOracleBaseAddress',
+  },
 };
