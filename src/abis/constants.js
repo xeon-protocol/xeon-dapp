66153811ea560244385c04f5d0c67bb4583a10e0
jonbray.eth <jon@xeon-protocol.io>
2024-07-21 13:23:35 -0400

    fix: update constants.js
    update to include correct testnet network information;
    expand JSON file to include multiple network constants;
    
    Signed-off-by: jonbray.eth <jon@xeon-protocol.io>

--- src/abis/constants.js
+++ src/abis/constants.js
@@ -1,10 +1,26 @@
 // global contract addresses
-export const Contstants = {
-  network: "0x56",
-  chainId: 56,
-  bscScan: "https://bscscan.com",
-  bscScanAPI: "https://api.bscscan.com",
-  onboardingUtilsContractAddress: "0x88232810891E015161D3e57C1E5A5DA2376407d5",
-  MockERC20FactoryContractAddress: "0x5A0d5390c45b49505C43A56DA4A4f89b93023F11",
-  WETH: "0x395cB7753B02A15ed1C099DFc36bF00171F18218",
+export const Constants = {
+  testnet: {
+    network: "Base Sepolia",
+    chainId: 84532,
+    baseSepoliaScan: "https://sepolia.basescan.org",
+    baseSepoliaScanAPI: "https://api-sepolia.basescan.org/api",
+    onboardingUtilsContractAddress: "0x88232810891E015161D3e57C1E5A5DA2376407d5",
+    MockERC20FactoryContractAddress: "0x5A0d5390c45b49505C43A56DA4A4f89b93023F11",
+    WETH: "0x395cB7753B02A15ed1C099DFc36bF00171F18218",
+  },
+  ethereum: {
+    network: "Ethereum",
+    chainId: 1,
+    baseMainnetScan: "https://etherscan.io",
+    baseMainnetScanAPI: "https://api.etherscan.io/api",
+    WETH: "0xMainnetWETHAddressHere",
+  },
+  base: {
+    network: "Base",
+    chainId: 8453,
+    baseMainnetScan: "https://basescan.org",
+    baseMainnetScanAPI: "https://api.basescan.org/api",
+    WETH: "0xMainnetWETHAddressHere",
+  }
 };
