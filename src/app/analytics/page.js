984ddc2068fbab9d947379dfa996f7bb351af51c
rtj99 <ryantjena@gmail.com>
2024-07-23 11:42:44 +0200

    fix : disable click events on wallet and analytics page

--- src/app/analytics/page.js
+++ src/app/analytics/page.js
@@ -1,12 +1,14 @@
 import TokenInformation from "@/components/Analytics/TokenInformation";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import UnderConstruction from "@/components/UnderConstruction";
 import React from "react";
 
 function Page() {
   return (
     <div className="pt-20">
       <Header />
+      <UnderConstruction />
       <TokenInformation />
       <Footer />
     </div>
