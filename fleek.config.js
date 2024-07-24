d0fc3fc4d59ad4aa4db74332578bc404ab57b590
Jon <jon@xeon-protocol.io>
2024-07-24 01:05:08 -0400

    chore: add fleek.config.js

---
+++ fleek.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@fleekxyz/cli').FleekConfig} */
+module.exports = {
+  sites: [
+    {
+      // "slug": "howling-potato-large",
+      slug: 'scarce-bit-crashing',
+      distDir: '/',
+      buildCommand: 'npm run build',
+    },
+  ],
+};
