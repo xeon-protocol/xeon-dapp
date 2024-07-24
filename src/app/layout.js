3d4097d7c9f0272037a316acd1d2f6926a883d4c
Jon <jon@xeon-protocol.io>
2024-07-23 22:08:44 -0400

    feat: update layout with SEO improvements and add meta image
    - add 'xeon-meta-image.png' to 'public/'
    - update RootLayout with enhanced metadata for SEO:
      - title: Xeon Protocol | Redefining DeFi
      - description: Unlock liquidity and manage risk seamlessly with Xeon Protocol, the future of DeFi. Use any ERC-20 token as collateral, lend tokens for a DeFi cash advance, or trade options & swaps using our next-gen hedging tools.
      - url: https://www.xeon-protocol.io
      - image: xeon-meta-image.png

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,8 @@ export const metadata = {
   title: 'Xeon Protocol | Redefining DeFi',
   description:
     'Unlock liquidity and manage risk seamlessly with Xeon Protocol, the future of DeFi. Use any ERC-20 token as collateral, lend tokens for a DeFi cash advance, or trade options & swaps using our next-gen hedging tools.',
+  url: 'https://www.xeon-protocol.io',
+  image: '/public/xeon-meta-image.png',
 };
 
 export default function RootLayout({ children }) {
@@ -20,6 +22,14 @@ export default function RootLayout({ children }) {
 
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.url + metadata.image} />
+        <meta property="og:url" content={metadata.url} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.url + metadata.image} />
       </head>
       <body>
         <ThirdwebProvider>
