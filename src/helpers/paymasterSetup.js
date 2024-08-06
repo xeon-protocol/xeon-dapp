d6ddb3c7f0e96acb6290dc94f5910719f70cbdf1
Jon <jon@xeon-protocol.io>
2024-08-06 18:16:13 -0400

    fix: properly wrap async function in paymasterSetup.js

--- src/helpers/paymasterSetup.js
+++ src/helpers/paymasterSetup.js
@@ -7,7 +7,6 @@ import { ABI, Constants } from '@/abi/constants';
 
 // Paymaster variables
 const rpcUrl = process.env.RPC_URL_BASE_SEPOLIA_PAYMASTER;
-
 const BASE_FACTORY_ADDRESS = '0x15Ba39375ee2Ab563E8873C8390be6f2E2F50232';
 const BASE_ENTRYPOINT_V06 = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
 const onboardingUtilsContractAddress =
@@ -23,27 +22,31 @@ const publicClient = createPublicClient({
   transport: http(rpcUrl),
 });
 
-// Creating smart account
-const simpleAccount = await privateKeyToSimpleSmartAccount(publicClient, {
-  privateKey: process.env.PRIVATE_KEY_PAYMASTER,
-  factoryAddress: BASE_FACTORY_ADDRESS,
-  entryPoint: BASE_ENTRYPOINT_V06,
-});
+async function setupPaymaster() {
+  // Creating smart account
+  const simpleAccount = await privateKeyToSimpleSmartAccount(publicClient, {
+    privateKey: process.env.PRIVATE_KEY_PAYMASTER,
+    factoryAddress: BASE_FACTORY_ADDRESS,
+    entryPoint: BASE_ENTRYPOINT_V06,
+  });
 
-// Create coinbase dev platform paymaster and plug into smart account client
-const cloudPaymaster = createPimlicoPaymasterClient({
-  chain: baseSepolia,
-  transport: http(rpcUrl),
-  entryPoint: BASE_ENTRYPOINT_V06,
-});
+  // Create coinbase dev platform paymaster and plug into smart account client
+  const cloudPaymaster = createPimlicoPaymasterClient({
+    chain: baseSepolia,
+    transport: http(rpcUrl),
+    entryPoint: BASE_ENTRYPOINT_V06,
+  });
 
-const smartAccountClient = createSmartAccountClient({
-  account: simpleAccount,
-  chain: baseSepolia,
-  bundlerTransport: http(rpcUrl),
-  middleware: {
-    sponsorUserOperation: cloudPaymaster.sponsorUserOperation,
-  },
-});
+  const smartAccountClient = createSmartAccountClient({
+    account: simpleAccount,
+    chain: baseSepolia,
+    bundlerTransport: http(rpcUrl),
+    middleware: {
+      sponsorUserOperation: cloudPaymaster.sponsorUserOperation,
+    },
+  });
 
-export { ABI, Constants, smartAccountClient, targetContracts, publicClient };
+  return { ABI, Constants, smartAccountClient, targetContracts, publicClient };
+}
+
+export default setupPaymaster;
