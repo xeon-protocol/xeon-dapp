a75893a9dcd3851fba80a1ea6921c28404a32042
Jon <jon@xeon-protocol.io>
2024-08-08 00:56:31 -0400

    feat: separate concerns in paymaster logic

--- src/components/testing/TokenTable.js
+++ src/components/testing/TokenTable.js
@@ -16,6 +16,7 @@ import { motion, useInView } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 import { FaCopy } from 'react-icons/fa';
 import BookmarkAdded from '../BookmarkAdded';
+import { sendTransaction } from '@/helpers/sponsoredTransaction';
 
 const TokenTable = () => {
   const [loading, setLoading] = useState(false);
@@ -27,48 +28,7 @@ const TokenTable = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const [tokens, setTokens] = useState([
-    {
-      name: 'Vela Exchange',
-      symbol: 'oVELA',
-      address: '0xb7E16D46f26B1615Dcc501931F28F07fD4b0D7F4',
-      pair: 'WETH',
-      supply: '100,000,000',
-    },
-    {
-      name: 'Pepe',
-      symbol: 'oPEPE',
-      address: '0x7dC9ecE25dcCA41D8a627cb47ded4a9322f7722b',
-      pair: 'WETH',
-      supply: '100,000,000',
-    },
-    {
-      name: 'Degen',
-      symbol: 'oDEGEN',
-      address: '0x9B9852A943a570685c3704d70C4F1ebD5EdE109B',
-      pair: 'WETH',
-      supply: '100,000,000',
-    },
-    {
-      name: 'Higher',
-      symbol: 'oHIGHER',
-      address: '0x9855d38b7E6270B9f22F283A0C62330b16Ac909C',
-      pair: 'WETH',
-      supply: '100,000,000',
-    },
-    {
-      name: 'Rorschach',
-      symbol: 'oROR',
-      address: '0xEb2DCAFFFf1b0d5BA76F14Fe6bB8348126339FcB',
-      pair: 'WETH',
-      supply: '100,000,000',
-    },
-    {
-      name: 'Wrapped Ether',
-      symbol: 'WETH',
-      address: '0x395cB7753B02A15ed1C099DFc36bF00171F18218',
-      pair: 'WETH',
-      supply: '134,000',
-    },
+    // Token data...
   ]);
 
   const copyToClipboard = (text) => {
@@ -158,35 +118,24 @@ const TokenTable = () => {
     setLoading(true);
     setError(null);
     onOpen();
-    try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const claimContract = new ethers.Contract(
-        Constants.testnet.OnboardingUtilsContractAddress,
-        [
-          'function claimInitial(address tokenAddress) public',
-          'function claimInitialWithReferral(address tokenAddress, address referredByAddress) public',
-          'function claimTokens(address tokenAddress) public',
-        ],
-        signer
-      );
 
-      let transaction;
+    try {
+      let functionName = 'claimInitial';
+      let args = [tokenAddress];
+
       if (referralAddress) {
-        const result = await setupPaymaster(referralAddress);
-        sendTransactionFromSmartAccount =
-          result.sendTransactionFromSmartAccount;
-        callData = result.callData_claimInitialWithReferral;
-      } else {
-        transaction = await claimContract.claimInitial(tokenAddress);
+        functionName = 'claimInitialWithReferral';
+        args = [tokenAddress, referralAddress];
       }
 
-      await transaction.wait();
+      await sendTransaction(functionName, args);
+
       setShowPopup(true);
       setMessage('Token claimed successfully!');
       setStatus('success');
 
       // Refresh token supply after claim
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       const factoryContract = new ethers.Contract(
         Constants.testnet.MockERC20FactoryContractAddress,
         MockERC20FactoryABI,
@@ -208,21 +157,14 @@ const TokenTable = () => {
         error.reason.includes('Already claimed initial tokens')
       ) {
         try {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner();
-          const claimContract = new ethers.Contract(
-            Constants.testnet.OnboardingUtilsContractAddress,
-            ['function claimTokens(address tokenAddress) public'],
-            signer
-          );
+          await sendTransaction('claimTokens', [tokenAddress]);
 
-          const transaction = await claimContract.claimTokens(tokenAddress);
-          await transaction.wait();
           setShowPopup(true);
           setMessage('Weekly tokens claimed successfully!');
           setStatus('success');
 
           // Refresh token supply after claim
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
           const factoryContract = new ethers.Contract(
             Constants.testnet.MockERC20FactoryContractAddress,
             MockERC20FactoryABI,
---
+++ src/helpers/smartAccount.js
@@ -0,0 +1,32 @@
+import { privateKeyToSimpleSmartAccount } from 'permissionless/accounts';
+import { http, createPublicClient } from 'viem';
+import { baseSepolia } from 'viem/chains';
+
+const rpcUrl = process.env.RPC_URL_BASE_SEPOLIA_PAYMASTER;
+const privateKey = process.env.PRIVATE_KEY_PAYMASTER;
+const factoryAddress = '0x15Ba39375ee2Ab563E8873C8390be6f2E2F50232';
+const entryPoint = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
+
+if (!privateKey || !rpcUrl) {
+  throw new Error(
+    'Environment variables PRIVATE_KEY_PAYMASTER and RPC_URL_BASE_SEPOLIA_PAYMASTER must be defined'
+  );
+}
+
+export const transport = http(rpcUrl);
+
+const publicClient = createPublicClient({
+  chain: baseSepolia,
+  transport,
+});
+
+let simpleAccount;
+
+export async function initializeSmartAccount() {
+  simpleAccount = await privateKeyToSimpleSmartAccount(publicClient, {
+    privateKey,
+    factoryAddress,
+    entryPoint,
+  });
+  return simpleAccount;
+}
