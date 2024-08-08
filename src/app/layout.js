ed2d28d2fd15a2ab0e40d74f4f0a99f717f54964
Jon <jon@xeon-protocol.io>
2024-08-07 22:57:41 -0400

    chore: update SEO metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
-import NetworkChecker from "@/components/ConnectWallet/NetworkChecker";
-import "./globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
-import { ThirdwebProvider } from "thirdweb/react";
+import NetworkChecker from '@/components/ConnectWallet/NetworkChecker';
+import './globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ThirdwebProvider } from 'thirdweb/react';
 
 export const metadata = {
-  title: "Xeon Protocol | Redefining DeFi",
-  description: 'Unlock liquidity and manage risk seamlessly with Xeon Protocol, the future of DeFi. Use any ERC-20 token as collateral, lend tokens for a DeFi cash advance, or trade options & swaps using our next-gen hedging tools.',
+  title: 'Xeon Protocol | Redefining DeFi',
+  description:
+    'Unlock liquidity and manage risk seamlessly with Xeon Protocol, the future of DeFi. Use any ERC-20 token as collateral, lend tokens for a DeFi cash advance, or trade options & swaps using our next-gen hedging tools.',
   url: 'https://www.app.xeon-protocol.io',
   image: '/public/xeon-meta-image.png',
 };
@@ -21,15 +22,15 @@ export default function RootLayout({ children }) {
 
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/public/favicon.ico" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.url + metadata.image} />
+        <meta property="og:image" content={metadata.image} />
         <meta property="og:url" content={metadata.url} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content={metadata.image} />
         <meta name="twitter:title" content={metadata.title} />
         <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.url + metadata.image} />
+        <meta name="twitter:image" content={metadata.image} />
       </head>
       <body>
         <ThirdwebProvider>
