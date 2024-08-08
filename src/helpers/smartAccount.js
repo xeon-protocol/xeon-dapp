0ec742309f05f59b5cd2fd6b14a0156df0c7b93f
Jon <jon@xeon-protocol.io>
2024-08-08 12:17:10 -0400

    fix: remove console check for env variables

--- src/helpers/smartAccount.js
+++ src/helpers/smartAccount.js
@@ -7,12 +7,6 @@ const privateKey = process.env.PRIVATE_KEY_PAYMASTER;
 const factoryAddress = '0x15Ba39375ee2Ab563E8873C8390be6f2E2F50232';
 const entryPoint = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
 
-if (!privateKey || !rpcUrl) {
-  throw new Error(
-    'Environment variables PRIVATE_KEY_PAYMASTER and RPC_URL_BASE_SEPOLIA_PAYMASTER must be defined'
-  );
-}
-
 export const transport = http(rpcUrl);
 
 const publicClient = createPublicClient({
