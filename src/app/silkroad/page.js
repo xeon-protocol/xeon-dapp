06afe6196ae3e72a1c06f1588add8e577e530034
rtj99 <ryantjena@gmail.com>
2024-09-04 23:29:05 +0200

    fix:pull tokens from uniswap

--- src/app/silkroad/page.js
+++ src/app/silkroad/page.js
@@ -6,66 +6,48 @@ import Header from "@/components/Header";
 import Layout from "@/components/Layout";
 import NoEvents from "@/components/NoEvents";
 import SocialPopup from "@/components/SocialPopup";
-import React from "react";
-import {TokenList} from "@uniswap/token-lists";
+import React, {useEffect, useState} from "react";
+
 import Loans from "@/components/Loans";
 
 export default function Home() {
-  const [showPositions, setShowPositions] = React.useState(false);
-  const [showDiscover, setShowDiscover] = React.useState(true);
-  const [showBookmarks, setShowBookmarks] = React.useState(false);
-  const [showSocials, setShowSocials] = React.useState(false);
-  const [showComingSoon, setShowComingSoon] = React.useState(false);
-  const [activeSideTab, setActiveSideTab] = React.useState("");
-  const [activeSection, setActiveSection] = React.useState("discover");
-  const [chainId, setChainId] = React.useState(1);
-  const [selectedToken, setSelectedToken] = React.useState("");
+  const [showPositions, setShowPositions] = useState(false);
+  const [showDiscover, setShowDiscover] = useState(true);
+  const [showBookmarks, setShowBookmarks] = useState(false);
+  const [showSocials, setShowSocials] = useState(false);
+  const [showComingSoon, setShowComingSoon] = useState(false);
+  const [showLoans, setShowLoans] = useState(false);
 
-  const [showLoans, setShowLoans] = React.useState(false);
-  console.log("schema", TokenList);
-  // NOTE: Mock token list with added chainId so that we can use it with teller
-  const tokenList = [
-    {
-      name: "Vela Exchange",
-      symbol: "oVELA",
-      address: "0xb7E16D46f26B1615Dcc501931F28F07fD4b0D7F4",
-      pair: "WETH",
-      supply: "100,000,000",
-      chainId: 555, //this is not a real chainId
-    },
-    {
-      name: "Pepe",
-      symbol: "oPEPE",
-      address: "0x7dC9ecE25dcCA41D8a627cb47ded4a9322f7722b",
-      pair: "WETH",
-      supply: "100,000,000",
-      chainId: 411,
-    },
-    {
-      name: "Degen",
-      symbol: "oDEGEN",
-      address: "0x9B9852A943a570685c3704d70C4F1ebD5EdE109B",
-      pair: "WETH",
-      supply: "100,000,000",
-      chainId: 666666666,
-    },
-    {
-      name: "Higher",
-      symbol: "oHIGHER",
-      address: "0x9855d38b7E6270B9f22F283A0C62330b16Ac909C",
-      pair: "WETH",
-      supply: "100,000,000",
-      chainId: 1, // this is not the real chainId as well
-    },
-    {
-      name: "Rorschach",
-      symbol: "oROR",
-      address: "0xEb2DCAFFFf1b0d5BA76F14Fe6bB8348126339FcB",
-      pair: "WETH",
-      supply: "100,000,000",
-      chainId: 8453, // used base chainId
-    },
-  ];
+  const [activeSideTab, setActiveSideTab] = useState("");
+  const [activeSection, setActiveSection] = useState("discover");
+  const [chainId, setChainId] = useState(1);
+  const [selectedToken, setSelectedToken] = useState("");
+  const [tokenList, setTokenList] = useState([]);
+
+  useEffect(() => {
+    const fetchTokenList = async () => {
+      try {
+        const response = await fetch(
+          "https://tokens-uniswap-org.ipns.dweb.link/"
+        );
+        if (response.ok) {
+          const data = await response.json();
+
+          setTokenList(data.tokens);
+        } else {
+          console.error(
+            "Failed to fetch token list, response not ok",
+            response.status
+          );
+        }
+      } catch (error) {
+        console.error("Failed to fetch token list", error);
+      }
+    };
+
+    fetchTokenList();
+  }, []);
+
   const handleSelectedToken = (inputAddress) => {
     const token = tokenList.find(
       (t) => t.address.toLowerCase() === inputAddress.toLowerCase()
@@ -79,6 +61,7 @@ export default function Home() {
       setShowLoans(false);
     }
   };
+
   return (
     <>
       <div className="bg-[#000] min-h-[100vh] px-8 pt-8 max-w-screen-2xl mx-auto">
@@ -107,8 +90,6 @@ export default function Home() {
               setShowComingSoon={setShowComingSoon}
               activeSection={activeSection}
               activeSideTab={activeSideTab}
-              setActiveSection={setActiveSection}
-              setActiveSideTab={setActiveSideTab}
               setSelectedToken={setSelectedToken}
               tokenList={tokenList}
               handleSelectedToken={handleSelectedToken}
